51b5bb59fa17ad073a122bd484453655c4da59a1
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-22 22:20:18 +0900

    improve:privateメソッド内にset_taskを作成、before_actionで対象アクションを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,47 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update)
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "タスク作成"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集完了"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
