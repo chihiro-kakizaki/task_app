aa48f689f60ec069d9e0fec98c1b526dc7dc041a
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-23 11:27:02 +0900

    add:titleカラムにnull制約追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_105049) do
+ActiveRecord::Schema.define(version: 2022_02_23_022238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "content"
   end
 
