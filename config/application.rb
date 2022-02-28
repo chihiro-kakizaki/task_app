8d794610bab40b14f5979c3bbe4d76d109f2a650
chihiro-kakizaki <kkzkchr@gmail.com>
2022-02-28 11:24:44 +0900

    improve:国際化の定義追加

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,8 @@ module TaskApp
                        request_specs: false
     end
 
-    config.time_zone = 'Tokyo'
+    config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
