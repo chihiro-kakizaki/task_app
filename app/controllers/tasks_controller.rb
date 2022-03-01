ae9c08ef6cae71ade6a48776b5854edb62503cc5
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-01 21:55:15 +0900

    fix:createコントローラ修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(task_params)
+    @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_path, notice: "タスクを作成しました"
     else
