68ed481610cd6c073d5fef1bce6c4c789e323d47
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-02 14:26:33 +0900

    add:パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
 
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 4 }
 
   has_many :tasks
 end
