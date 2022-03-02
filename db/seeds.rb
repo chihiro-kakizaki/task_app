3c9656947af3d88c702e28e5f443d44b5a400db9
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-02 13:15:41 +0900

    add:最初のユーザをseedで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: '1番目のテストユーザー',
+  email: 'test1@gmail.com',
+  password_digest: 'test1'
+)
