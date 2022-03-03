b7ecf991dd6c4904c1034275385ae75b7a67f708
chihiro-kakizaki <kkzkchr@gmail.com>
2022-03-03 10:23:27 +0900

    add:adminのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
   resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
+  namespace :admin do
+    resources :users
+  end
 end
